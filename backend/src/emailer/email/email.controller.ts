f2ec1d2709d32d63e738e9a7521858c5dc2bef0b
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-08-29 20:16:28 -0600

    remove unnecessary code

--- backend/src/emailer/email/email.controller.ts
+++ backend/src/emailer/email/email.controller.ts
@@ -1,32 +1,7 @@
-import { Controller, Post, HttpCode, HttpStatus, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { EmailService } from './email.service'
 
 @Controller('email')
 export class EmailController {
     constructor(private emailService: EmailService) {}
-
-
-
-  @Post('send-verification')
-  @HttpCode(HttpStatus.OK)
-  async sendVerificationEmail() {
-    try {
-      await this.emailService.sendVerificationEmail('franciscoguillermo.hdez@gmail.com', '111-111', 'Activa tu cuenta con el siguiente OTP');
-      
-      return {
-        success: true,
-        message: 'Verification email sent successfully'
-      };
-    } catch (error) {
-      return {
-        success: false,
-        error: error.message
-      };
-    }
-  }
-
-  @Get('send-verification')
-  public hi() {
-    return 'Hi'
-  }
 }
