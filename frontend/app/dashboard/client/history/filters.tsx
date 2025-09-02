ec45563b0c9c09b0d9718759b9542e7d7952f2ee
Francisco Guillermo Hernandez <franciscoguillermo.hdez@gmail.com>
2025-09-01 21:49:00 -0600

    Add rage filters: from, to

--- frontend/app/dashboard/client/history/filters.tsx
+++ frontend/app/dashboard/client/history/filters.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { ChevronDownIcon, Calendar as CalendarIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar';
+import { DateRange } from 'react-day-picker';
 
 import { Label } from '@/components/ui/label';
 import {
@@ -12,94 +13,82 @@ import {
 import { addDays, differenceInDays, subDays, format } from 'date-fns';
 import { es } from 'date-fns/locale';
 
-const getLocalizedMonth = (date: Date, locale = es) => {
-  return format(date, 'MMMM', { locale });
-};
+const getLocalizedMonth = (date: Date, locale = es) => format(date, 'MMMM', { locale });
 
-type Filter = {
-  range?: { 
-      from: Date; 
-      to: Date 
-    }
-}
+export type Filter = {
+  range?: DateRange;
+};
 
 type ApplyFiltersCallBack = (filter: Filter) => void;
 type DownloadOrdersCallback = () => void;
-export type FiltersPageProps = { 
-  applyFiltersCallBack: ApplyFiltersCallBack, 
-  downloadOrdersCallback: DownloadOrdersCallback 
-}
+export type FiltersPageProps = {
+  applyFiltersCallBack: ApplyFiltersCallBack;
+  downloadOrdersCallback: DownloadOrdersCallback;
+};
 
-export default function FiltersPage({ applyFiltersCallBack, downloadOrdersCallback } : FiltersPageProps) { 
-
+export default function FiltersPage({
+  applyFiltersCallBack,
+  downloadOrdersCallback,
+}: FiltersPageProps) {
   const [open, setOpen] = React.useState(false);
-  const [date, setDate] = React.useState<{
-        from: Date | undefined;
-        to?: Date | undefined;
-      }>({
-        from: subDays(new Date(), 7),
-        to: new Date(),
-      });
+  const [date, setDate] = React.useState<DateRange>({
+    from: subDays(new Date(), 7),
+    to: new Date(),
+  });
+  const handleFilters = () => applyFiltersCallBack({ range: date });
+  const handleDownload = () => downloadOrdersCallback();
 
-
-    const handleFilters = () => {
-      applyFiltersCallBack({})
-    };
-
-    const handleDownload = () => {
-      downloadOrdersCallback();
-    }
-
-    return (
-         <div className="flex flex-row gap-3 mb-[20px]">
-        <div className="flex flex-col gap-3">
-          {/* <Label htmlFor="date" className="px-1">  </Label> */}
-          <Popover open={open} onOpenChange={setOpen}>
-            <PopoverTrigger asChild>
-              <Button
-                variant="outline"
-                id="date"
-                className="w-48 justify-between font-normal h-[40px] bg-white"
-              >
-                {date?.from && date?.to
-                  ? `${getLocalizedMonth(date.from)} - ${getLocalizedMonth(
-                      date.to
-                    )}`
-                  : 'Seleccione el rango de fecha'}
-                <CalendarIcon />
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent
-              className="w-auto overflow-hidden p-0"
-              align="start"
+  return (
+    <div className="flex flex-row gap-3 mb-[20px]">
+      <div className="flex flex-col gap-3">
+        {/* <Label htmlFor="date" className="px-1">  </Label> */}
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              id="date"
+              className="w-48 justify-between font-normal h-[40px] bg-white"
             >
-              <Calendar
-                required
-                mode="range"
-                captionLayout={'dropdown-months'}
-                defaultMonth={date.from}
-                numberOfMonths={2}
-                selected={date}
-                onSelect={(e) => {
-                  setDate(e);
-                  if (e.from && e.to) {
-                    const daysDifference = differenceInDays(e.from, e.to);
-                    console.log(daysDifference);
-                    // setOpen(false);
-                  }
-                }}
-                timeZone="America/El_Salvador"
-                locale={es}
-                showOutsideDays={false}
-                className="rounded-lg border shadow-sm"
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
-        <Button onClick={handleFilters}>Buscar</Button>
-        <Button onClick={handleDownload} variant={'outline'} className="bg-white h-[39px]">
-          Descargar órdenes
-        </Button>
+              {date?.from && date?.to
+                ? `${getLocalizedMonth(date.from)} - ${getLocalizedMonth(
+                    date.to
+                  )}`
+                : 'Seleccione el rango de fecha'}
+              <CalendarIcon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+            <Calendar
+              required
+              mode="range"
+              captionLayout={'dropdown-months'}
+              defaultMonth={date.from}
+              numberOfMonths={2}
+              selected={date}
+              onSelect={(e) => {
+                setDate(e);
+                if (e.from && e.to) {
+                  const daysDifference = differenceInDays(e.from, e.to);
+                  console.log(daysDifference);
+                  // setOpen(false);
+                }
+              }}
+              timeZone="America/El_Salvador"
+              locale={es}
+              showOutsideDays={false}
+              className="rounded-lg border shadow-sm"
+            />
+          </PopoverContent>
+        </Popover>
       </div>
-    );
+      <Button onClick={handleFilters}>Buscar</Button>
+      <Button
+        onClick={handleDownload}
+        variant={'outline'}
+        className="bg-white h-[39px]"
+      >
+        Descargar órdenes
+      </Button>
+    </div>
+  );
 }
